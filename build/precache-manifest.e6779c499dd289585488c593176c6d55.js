f92e3ceb476055be4fa488259a5b4b8fb3a5b359
yuyuanyuan <yuyuanyuan_2012@126.com>
2020-01-02 23:36:25 +0800

    first github start

---
+++ build/precache-manifest.e6779c499dd289585488c593176c6d55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33e9d251a425543085ef31df243d1d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4437e40ec9649dba1d16",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23e1f6f5684e40bff9e1",
+    "url": "/static/js/2.0bc93c31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0bc93c31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4437e40ec9649dba1d16",
+    "url": "/static/js/main.0e9b4cad.chunk.js"
+  },
+  {
+    "revision": "b7b94a1e7b8d1102b723",
+    "url": "/static/js/runtime-main.0e715f4c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
